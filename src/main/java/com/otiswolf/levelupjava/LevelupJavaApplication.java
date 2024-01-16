dfe7d239a5c945cc3f3869b35e15b7325f743ea4
Otis Wolf <wolfotis@gmail.com>
2024-01-15 22:26:56 -0600

    initial commit

---
+++ src/main/java/com/otiswolf/levelupjava/LevelupJavaApplication.java
@@ -0,0 +1,13 @@
+package com.otiswolf.levelupjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelupJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelupJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otiswolf/levelupjava/LevelupJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otiswolf.levelupjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelupJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
